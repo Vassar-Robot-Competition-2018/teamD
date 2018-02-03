04d97591399576a67fbed760d89fe583af8c427f
JTSnyder <jonsnyder@vassar.edu>
2018-02-03 13:22:53 -0500

    Pixy Cam Servo
    This program is for a servo attached to the Pixy Cam, causing the camera to sweep back and forth in search of colored blocks.

---
+++ Servo_Sweep/Servo_Sweep.ino
@@ -0,0 +1,38 @@
+// This program is for a servo attached to the Pixy Cam, causing the camera to sweep back
+// and forth in search of colored blocks. 
+
+#include <Servo.h>
+
+Servo camServo;  // create servo object to control a servo
+
+int pos = 0,    // variable to store the servo position
+    dir = 0;    // dictates direction of 'camServo' rotation
+
+void setup() {
+  camServo.attach(12);  // attaches the servo on pin 12 to the servo object
+}
+
+void loop() {
+
+  if (dir == 0)   // dir 0 sweeps from left to right
+  {
+    pos += 1;
+    camServo.write(pos);
+    delay(20);
+    if (pos == 180)
+    {
+      dir = 1;
+    }
+  }
+  else if (dir == 1) // dir 1 sweeps from right to left
+  {
+    pos -= 1;
+    camServo.write(pos);
+    delay(20);
+    if (pos == 0)
+    {
+      dir = 0;
+    }
+  }
+}
+
