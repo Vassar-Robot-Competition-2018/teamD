c94c057ec292d9088159f9b0925f3c9b44bc1f19
JTSnyder <jonsnyder@vassar.edu>
2018-02-19 09:46:14 -0500

    Code for identifying home block

---
+++ Main_Milestone_7/Functions.ino
@@ -0,0 +1,435 @@
+// Functions
+//    ColorSensor()
+//    WhiteQRE()
+//    MotorUpdate(double, double)
+//    StateCheck()
+//    CheckBlocks()
+//    BlockColor()
+//    PixyPID()
+//    WheelPID()
+//    DriveForward()
+//    DriveReverse()
+//    LeftTurn()
+//    RightTurn()
+//    DriveStop()
+//    Sweep()
+//    IR_Short()
+//    IR_Long()
+//    BlockComplete()
+
+void PID_Pixy_Wheels() {
+  if (irLongDist <= minDist) {
+
+    digitalWrite(WHITE, HIGH);    // LED
+    pixyError = pixySP - max_X;
+    //Serial.print(pixyError: ";
+    //Serial.println(pixyError);
+
+    wheelAdj = wheelKp * pixyError + wheelKd * (pixyError - lastPixyError);
+    lastPixyError = pixyError;
+    speedL = int(baseSpeedL + wheelAdj);   // not sure yet if it's L minus adjust or R minus adjust
+    speedR = int(baseSpeedR - wheelAdj);
+
+    if (speedL > maxSpeedL)
+      speedL = maxSpeedL;
+
+    else if (speedL < minSpeedL)
+      speedL = minSpeedL;
+
+    if (speedR > maxSpeedR)
+      speedR = maxSpeedR;
+
+    else if (speedR < minSpeedR)
+      speedR = minSpeedR;
+
+    MotorUpdate(speedL, speedR);
+  }
+  else
+    digitalWrite(WHITE, LOW);    // LED
+}
+
+//checks floor color sensor (up to 20 times per second)
+void ColorSensor() {
+  //uint16_t clear, red, green, blue;
+  int clear, red, green, blue;
+
+  tcs.setInterrupt(false);      // turn on LED
+  //delay(60);  // takes 50ms to read
+  tcs.getRawData(&red, &green, &blue, &clear);
+  tcs.setInterrupt(true);  // turn off LED
+
+  //  Serial.print("C:\t"); Serial.print(clear);
+  //  Serial.print("\tR:\t"); Serial.print(red);
+  //  Serial.print("\tG:\t"); Serial.print(green);
+  //  Serial.print("\tB:\t"); Serial.println(blue);
+
+  if (Cgray == 0) {
+    ColorCalibration(clear, red, green, blue);
+  }
+
+  WhiteCheck(clear);
+  Quadrant(red, green, blue);
+}
+
+
+void SetHome(int r, int g, int b) {
+  if ((homeQ == 0) && (quadrant != 0)) {
+    homeQ = quadrant;
+  }
+}
+
+
+void Quadrant(int r, int g, int b) {
+  if (r > (g + b))  //red
+    quadrant = 1;
+
+  else if ((r + g) > (4 * b))  //yellow
+    quadrant = 2;
+
+  else if ((3 * g) > ((r + b) * 2))  //green
+    quadrant = 3;
+
+  else if (b > (2 * r))  //blue
+    quadrant = 4;
+
+  SetHome(r, g, b);
+
+  if (quadrant != quadCheck) {
+    prev_quadrant = quadCheck;
+    quadCheck = quadrant;
+  }
+
+  Serial.print("Quadrant color: ");
+  Serial.println(quadrant);
+}
+
+
+void ColorCalibration(int c, int r, int g, int b) {
+  Cgray = c;
+  Rgray = r;
+  Ggray = g;
+  Bgray = b;
+
+  Serial.print("Color calibration complete");
+}
+
+
+void WhiteCheck(int c) {
+  if (c > (4 * Cgray)) {  // maybe 4*Cgray when white tape is used
+    Serial.println("White Detected");
+
+    DriveReverse();
+    delay(1000);
+
+    RightTurn();
+    delay(1000);
+  }
+}
+
+
+//void WhiteQRE() {
+//  QRE_Val = analogRead(QRE_Pin);
+//  if (QRE_Val < 100) {
+//    digitalWrite(WHITE, HIGH); //turns on white LED
+//
+//    DriveReverse();
+//    delay(1000);
+//
+//    RightTurn();
+//    delay(2000);
+//  }
+//}
+
+
+void MotorUpdate(double L, double R) {
+  double servoL_speed = (L * (-0.9)) + 92;
+  double servoR_speed = (R * 0.9 ) + 92;
+  servoL.write(servoL_speed);
+  servoR.write(servoR_speed);
+}
+
+
+void StateCheck() {
+  if (state == 0) { // if searching for block
+    DriveForward();
+    // Sweep();
+  }
+}
+
+
+void CheckBlocks() {
+  static int i = 0;
+  uint16_t blocks;
+  char buf[32];
+
+  maxJ = 0;
+  maxSig = 0;
+  max_X = 0;
+  max_Y = 0;
+  maxWidth = 0;
+  maxHeight = 0;
+
+  blocks = pixy.getBlocks();
+
+  if (blocks) { //if a color sig is detected by pixy cam
+    for (j = 0; j < blocks; j++) { //find the largest object that fits the signature
+      prod = pixy.blocks[j].width * pixy.blocks[j].height;
+      if (prod > maxProd)
+        maxJ = j;
+    }
+  }
+
+  if ((pixy.blocks[maxJ].width > minWidth) && ((pixy.blocks[maxJ].width < lineWidth) && (pixy.blocks[maxJ].height < lineHeight))) { // if color sig is big enough and object is detected by the IR sensor
+    maxSig = pixy.blocks[maxJ].signature;
+    max_X = pixy.blocks[maxJ].x;
+    max_Y = pixy.blocks[maxJ].y;
+    maxHeight = pixy.blocks[maxJ].height;
+    maxWidth = pixy.blocks[maxJ].width;
+
+    BlockColor();
+    PID_Pixy_Wheels();
+    //Sweep();
+    // PixyPID();
+    // WheelPID();
+  }
+
+  else {
+    //Serial.println("None");
+    digitalWrite(RED, LOW);    // LED
+    digitalWrite(YELLOW, LOW);    // LED
+    digitalWrite(GREEN, LOW);    // LED
+    digitalWrite(BLUE, LOW);    // LED
+    state = 0;
+    StateCheck();
+  }
+
+  //  Serial.print("Block Position: (");
+  //  Serial.print(max_X, max_Y);
+  //  Serial.println(")");
+  //  Serial.print("Block Dimensions: ");
+  //  Serial.print(maxWidth);
+  //  Serial.print(" x ");
+  //  Serial.println(maxWidth);
+
+  Serial.print("maxWidth: "); Serial.println(maxWidth);
+  Serial.print("irLongDist: "); Serial.println(irLongDist);
+
+  //  if ((irLongDist < stopDist) && (maxWidth > stopWidth)) {
+  //    state = 1;
+  // }
+
+  delay(30);
+}
+
+void BlockColor () {
+  if (maxSig == 1) {
+    //Serial.println("Red");
+    digitalWrite(RED, HIGH);    // LED
+    digitalWrite(YELLOW, LOW);    // LED
+    digitalWrite(GREEN, LOW);    // LED
+    digitalWrite(BLUE, LOW);    // LED
+  }
+
+  else if (maxSig == 2) {
+    //Serial.println("Yellow");
+    digitalWrite(RED, LOW);    // LED
+    digitalWrite(YELLOW, HIGH);    // LED
+    digitalWrite(GREEN, LOW);    // LED
+    digitalWrite(BLUE, LOW);    // LED
+  }
+
+  else if (maxSig == 3) {
+    //Serial.println("Green");
+    digitalWrite(RED, LOW);    // LED
+    digitalWrite(YELLOW, LOW);    // LED
+    digitalWrite(GREEN, HIGH);    // LED
+    digitalWrite(BLUE, LOW);    // LED
+  }
+
+  else if (maxSig == 4) {
+    //Serial.println("Blue");
+    digitalWrite(RED, LOW);    // LED
+    digitalWrite(YELLOW, LOW);    // LED
+    digitalWrite(GREEN, LOW);    // LED
+    digitalWrite(BLUE, HIGH);    // LED
+  }
+}
+
+
+void PixyPID() {
+
+  pixyError = pixySP - max_X;
+  //Serial.print(pixyError: ";
+  //Serial.println(pixyError);
+
+  servoAdj = pixyKp * pixyError + pixyKd * (pixyError - lastPixyError);
+  lastPixyError = pixyError;
+  pos = lastPos + servoAdj;
+  lastPos = pos;
+
+  if (pos > servoMax)
+    pos = servoMax;
+
+  else if (pos < servoMin)
+    pos = servoMin;
+
+  camServo.write(pos);
+  delay(20);
+}
+
+
+void WheelPID() {
+  servoError = servoSP - pos;
+  //Serial.print("servoError: ");
+  //Serial.println(servoError);
+
+  wheelAdj = wheelKp * servoError + wheelKd * (servoError - lastServoError);
+  lastServoError = servoError;
+  speedL = baseSpeedL + wheelAdj;   // not sure yet if it's L minus adjust or R minus adjust
+  speedR = baseSpeedR - wheelAdj;
+
+  if (speedL > maxSpeedL)
+    speedL = maxSpeedL;
+
+  else if (speedL < minSpeedL)
+    speedL = minSpeedL;
+
+  if (speedR > maxSpeedR)
+    speedR = maxSpeedR;
+
+  else if (speedR < minSpeedR)
+    speedR = minSpeedR;
+
+  MotorUpdate(speedL, speedR);
+}
+
+
+void DriveForward() {
+  speedL = 50;
+  speedR = 50;
+  MotorUpdate(speedL, speedR);
+}
+
+
+void DriveReverse() {
+  speedL = -50;
+  speedR = -50;
+  MotorUpdate(speedL, speedR);
+}
+
+
+void LeftTurn() {
+  speedL = -50;
+  speedR = 50;
+  MotorUpdate(speedL, speedR);
+}
+
+
+void RightTurn() {
+  speedL = 50;
+  speedR = -50;
+  MotorUpdate(speedL, speedR);
+}
+
+
+void DriveStop() {
+  speedL = 0;
+  speedR = 0;
+  MotorUpdate(speedL, speedR);
+}
+
+
+void Sweep() {
+  if (dir == 0)   // dir 0 sweeps from left to right
+  {
+    pos += 2;
+    camServo.write(pos);
+    delay(10);
+    if (pos >= servoMax)
+    {
+      dir = 1;
+    }
+  }
+
+  else if (dir == 1) // dir 1 sweeps from right to left
+  {
+    pos -= 2;
+    camServo.write(pos);
+    delay(10);
+    if (pos <= servoMin)
+    {
+      dir = 0;
+    }
+  }
+}
+
+
+void IR_Short() {
+  irShort = analogRead(IR_SHORT);
+
+  if ((irShort > 100) && (irShort < 700)) {
+    irShortDist = 3187.58 / irShort - 1.6016;
+  }
+  else {
+    irShortDist = 100;
+
+    //    Serial.print("Short IR Value: ");  // returns it to the serial monitor
+    //    Serial.print(irShort);
+    //    Serial.print("     Distance: ");  // returns it to the serial monitor
+    //    Serial.print(irShortDist);
+    //    Serial.println(" cm");
+    //    Serial.println();
+  }
+}
+
+
+void IR_Long() {
+  irLong = analogRead(IR_LONG);
+
+  if ((irLong > 70) && (irLong < 700)) {
+    irLongDist = 6206.62 / irLong - 0.1826;
+  }
+  else {
+    irLongDist = 100;
+  }
+
+  //  Serial.print("Long IR Value: ");  // returns it to the serial monitor
+  //  Serial.print(irLong);
+  //  Serial.print("     Distance: ");  // returns it to the serial monitor
+  //  Serial.print(irLongDist);
+  //  Serial.println(" cm");
+}
+
+
+void GrabBlock() {
+  blockServoL.write(135);
+  blockServoR.write(45);
+  delay(1000);
+}
+
+void ReleaseBlock() {
+  blockServoL.write(135);
+  blockServoR.write(45);
+  DriveReverse();
+  delay(2000);
+  RightTurn();
+  delay(2000);
+}
+
+void HoldingBlock() {
+  if (irLongDist <= captured)
+    DriveForward();
+}
+
+void BlockComplete() {
+  speedL = 0;
+  speedR = 0;
+  MotorUpdate(speedL, speedR);
+  BlockColor();
+  digitalWrite(WHITE, HIGH);    // LED
+  Serial.print("maxWidth: "); Serial.println(maxWidth);
+  Serial.print("irLongDist: "); Serial.println(irLongDist);
+  delay(5000);
+}
+
+
---
+++ Main_Milestone_7/Main_Milestone_7.ino
@@ -0,0 +1,132 @@
+#include <SPI.h>
+#include <Pixy.h>
+#include <Servo.h>
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+Pixy pixy;          // main Pixy object
+Servo servoL;       // left motor
+Servo servoR;       // right motor
+Servo blockServoL;  // left block servo
+Servo blockServoR;  // right block servo
+Servo camServo;     // pixy servo
+
+//Declares constants
+const int QRE_Pin = A0,     // connects line sensor to analog pin 0
+          IR_SHORT = A1,    // A41 has shorter range: 4-30cm
+          IR_LONG = A2,     // A21 has longer range: 10-80cm
+          RED = 2,          // assigns Red LED to pin 1
+          YELLOW = 3,       // assigns Yellow LED to pin 2
+          GREEN = 4,        // assigns Green LED to pin 3
+          BLUE = 5,         // assigns Blue LED to pin 4
+          WHITE = 6,        // assigns White LED to pin 5
+          captured = 10,    // IR distance that recognized block captured
+          minDist = 45,     // min block width for CheckBlock and to trigger PID
+          minWidth = 20,    // minimum color block width
+          stopWidth = 40,   // block width required for completion of milestone 3
+          stopDist = 35,    // IR distance required for completion of milestone 3
+          pixySP = 160,     // pixy cam setpoint
+          servoSP = 90,     // servo setpoint
+          servoMin = 45,    // min pixy servo position
+          servoMax = 135;   // max pixy servo position
+
+double baseSpeedL = 50,
+       baseSpeedR = 50,
+       pixyKp = 0.05,
+       pixyKi = 0.0,
+       pixyKd = 0.02,
+       wheelKp = 0.5,
+       wheelKi = 0.0,
+       wheelKd = 0.3,
+       wheelAdj = 0;
+
+float irShort,
+      irShortDist,
+      irLong,
+      irLongDist = 100;
+
+int Cgray,
+    Rgray,
+    Ggray,
+    Bgray,
+    QRE_Val = 0,      // white line sensor value
+    state = 0,
+    j,
+    maxJ,
+    prod,
+    maxProd,
+    maxSig,
+    max_X,
+    max_Y,
+    maxWidth = 0,
+    maxHeight,
+    lineWidth = 230,
+    lineHeight = 150,
+    pixyError,
+    lastPixyError = 0,
+    pos = 90,    // variable to store the servo position
+    lastPos = 90,
+    servoAdj,
+    servoError,
+    lastServoError,
+    dir = 0,
+    sweep = 0,
+    quadrant = 0,
+    quadCheck = 0,
+    prev_quadrant,
+    homeQ = 0,
+    speedL = 0.0,
+    speedR = 0.0,
+    minSpeedL = 0,
+    minSpeedR = 0,
+    maxSpeedL = 100,
+    maxSpeedR = 100;
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+void setup() {
+  servoL.attach(8);  // attaches left wheel servo on pin 8
+  servoR.attach(9);  // attaches right wheel servo on pin 9
+  blockServoL.attach(10); // attaches left block servo on pin 10
+  blockServoR.attach(11); // attaches right block servo on pin 11
+  camServo.attach(12);  // attaches the servo on pin 12 to the servo object
+
+  // sets servo initial positions
+  camServo.write(90);
+  blockServoL.write(45);
+  blockServoR.write(135);
+  servoL.write(92);
+  servoR.write(92);
+  delay(1000);
+
+  pinMode(RED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+
+  Serial.begin(9600);
+  tcs.begin();
+  pixy.init();
+}
+
+
+void loop() {
+  ColorSensor(); // includes ColorCalibration, WhiteCheck, and Quadrant functions
+  IR_Short();
+  IR_Long();
+  
+  if (irShortDist > captured) {
+
+    CheckBlocks();
+    //    StateCheck();
+  }
+
+  else
+  { 
+   // IR_Long();
+    DriveForward();
+    digitalWrite(WHITE, LOW);    // LED
+    //  BlockComplete();
+  }
+}
+
