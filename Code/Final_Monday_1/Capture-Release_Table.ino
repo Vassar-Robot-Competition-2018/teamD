420da0f63af679db07f20fadf130390b9a917ef5
JTSnyder <jonsnyder@vassar.edu>
2018-05-14 10:03:51 -0400

    Monday v1

---
+++ Code/Final_Monday_1/Capture-Release_Table.ino
@@ -0,0 +1,16 @@
+// set capture-release variables based on Home
+
+void Cap_Rel_Table() {
+  Neutral_1 += Home;
+  if (Neutral_1 > 4) {
+    Neutral_1 -= 4;
+  }
+  else Enemy += Home;
+  if (Enemy > 4) {
+    Enemy -= - 4;
+  }
+  Neutral_2 += Home;
+  if (Neutral_2 > 4) {
+    Neutral_2 -= 4;
+  }
+}
